fba3afe2ddd5286122871c6dee7c8a275a1e50f3
gorushkin <gorushkin@gmail.com>
2022-02-13 16:05:21 +0300

    добавил scanDir

--- __tests__/archiver.test.js
+++ __tests__/archiver.test.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 import Archiver from '../src/archiver';
-import { resetDir, getDirContent } from './helpers';
+import { resetDir, getDirContent, scanDir } from './helpers';
 import unzipper from 'unzipper';
 
 const __filename = fileURLToPath(import.meta.url);
@@ -19,11 +19,20 @@ const testData = [
   {
     testGroupName: 'Pack single file',
     folderName: 'test1',
-    inputName: getPathToFixtures('text.txt'),
-    output: 'archive.zip',
-    expectedFile: getFileFromFixtures('text.txt'),
-    result: ['archive.zip'],
+    inputName: getPathToFixtures('test1/text.txt'),
+    resultFolder: getPathToTempDir('test1'),
+    // expectedFile: getFileFromFixtures('test1'),
+    result: 'test1/text.txt',
+    expectedArchiveName: 'archive.zip',
   },
+  // {
+  //   testGroupName: 'Pack directory',
+  //   folderName: 'test2',
+  //   inputName: getPathToFixtures('directory'),
+  //   output: 'archive.zip',
+  //   expectedFile: getFileFromFixtures('text.txt'),
+  //   result: ['archive.zip'],
+  // },
 ];
 
 describe('Packing tests', () => {
@@ -33,29 +42,39 @@ describe('Packing tests', () => {
 
   describe.each(testData.map((item) => item))(
     '$testGroupName',
-    ({ testGroupName, folderName, inputName, output, expectedFile, result }) => {
+    ({
+      testGroupName,
+      folderName,
+      inputName,
+      expectedArchiveName,
+      resultFolder,
+      expectedFile,
+      result,
+    }) => {
       let dirContent;
+      let expectedDirContent;
       beforeAll(async () => {
         await fs.promises.mkdir(getPathToTempDir(folderName));
         await Archiver.pack(inputName, getPathToTempDir(folderName), {
           archiveName: 'archive.zip',
           level: 1,
         });
-        dirContent = await getDirContent(getPathToTempDir(folderName));
+
+        [dirContent] = await getDirContent(resultFolder);
       });
 
       test('file was packed successfuly', async () => {
-        expect(JSON.stringify(dirContent) === JSON.stringify(result));
+        expect(dirContent).toEqual(expectedArchiveName);
       });
 
-      test('archive content is correct', async () => {
-        const [archive] = await getDirContent(getPathToTempDir(folderName));
+      // test('archive content is correct', async () => {
+      //   const [archive] = await getDirContent(getPathToTempDir(folderName));
 
-        const directory = await unzipper.Open.file(getPathToTempDir(folderName, archive));
-        const file = directory.files[0];
-        const content = await file.buffer();
-        expect(content.toString() === expectedFile.toString());
-      });
+      //   const directory = await unzipper.Open.file(getPathToTempDir(folderName, archive));
+      //   const file = directory.files[0];
+      //   const content = await file.buffer();
+      //   expect(content.toString() === expectedFile.toString());
+      // });
     }
   );
 });
--- __tests__/helpers.js
+++ __tests__/helpers.js
@@ -1,14 +1,12 @@
 import fs from 'fs';
+import path, { dirname } from 'path';
 
 export const resetDir = async (dirPath) => {
   try {
-    const stat = await fs.promises.stat(dirPath);
-    if (stat) {
-      await fs.promises.rmdir(dirPath, { recursive: true });
-      await fs.promises.mkdir(dirPath);
-    }
-  } catch (error) {
-    console.log('error: ', error);
+    if (fs.existsSync(dirPath)) await fs.promises.rmdir(dirPath, { recursive: true });
+    await fs.promises.mkdir(dirPath);
+  } catch (e) {
+    console.log('An error occurred.');
   }
 };
 
@@ -19,4 +17,45 @@ export const getDirContent = async (path) => {
   } catch (error) {
     console.log('error: ', error);
   }
+};
+
+export const getDirectory = async (path) => {
+  try {
+    const files = await fs.promises.readdir(path);
+    console.log('files: ', files);
+    return files;
+  } catch (error) {
+    console.log('error: ', error);
+  }
+};
+
+export const scanDir = async (target, view = 'simple') => {
+  const getFileStat = async (filename) => {
+    try {
+      const stat = await fs.promises.stat(filename);
+      const name = view === 'simple' ? path.basename(filename) : filename;
+      if (stat.isFile()) return { ...(view === 'full' && { type: 'file' }), name };
+      if (stat.isDirectory()) {
+        const childrens = await readDir(filename);
+        return { ...(view === 'full' && { type: 'directory' }), name, childrens };
+      }
+    } catch (error) {
+      console.log('error: ', error);
+    }
+  };
+
+  const readDir = async (filename) => {
+    try {
+      const files = await fs.promises.readdir(filename);
+      const filesStats = await Promise.all(
+        files.map(async (item) => await getFileStat(path.join(filename, item)))
+      );
+      return filesStats;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  const result = await getFileStat(target);
+  return result;
 };
